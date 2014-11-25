087243da2e80c104d8c159c4bbf6d8ad5f8ead88
jorgitopeke <jorgito.c.2011@gmail.com>
2014-11-25 17:51:14 -0600

    Create CalculaPrestacionesExt.cs

---
+++ proyectofinal/CalculaPrestacionesExt.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Prestaciones
+{
+    class CalculaPrestExt
+    {
+        public double tiempoExtraordinario(int horas, int s)
+        {
+            int sh = s/8;
+            if (horas <= 9)
+                return sh;
+            else
+            {
+                int horast = horas - 9;
+                double dob = (sh * 2) * 9;
+                double trip = (sh * 3) * horast;
+
+                return dob + trip;
+            }
+        }
+    }
+}

bb09ebea300bb4935b8e1a86ba144c4a3189e0a0
CristinaGlz <cgm00001@gmail.com>
2014-11-24 21:12:40 -0600

    ingreso

---
+++ proyectofinal/Ingreso.cs
@@ -0,0 +1,26 @@
+﻿namespace proyectofinal{
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+	public class Ingreso
+	{
+	
+		public double exentos(double dia,double tipo,double aguinaldo, String zona){
+			double exe;
+			double zon;
+			 
+				if (zona =="A")
+				{
+					zon=67.29;
+				}else{
+					zon=63.77;
+				}
+			exe = aguinaldo * zon;
+			return Math.Round(exe,2);
+		}
+	}
+}
+
---
+++ proyectofinal/Pruebas.cs
@@ -0,0 +1,27 @@
+﻿namespace proyectofinal
+{
+using NUnit.Framework;
+using System;
+
+
+	[TestFixture ()]
+	public class Pruebas
+	{
+
+				
+		[Test ()]
+		public  void TestCase ()
+		{
+			Ingreso ing = new Ingreso ();
+			Assert.AreEqual(2018.7,ing.exentos(100,30,30,"A"));
+		}
+		[Test ()]
+		public  void TestCase1 ()
+		{
+			Ingreso ing = new Ingreso ();
+			Assert.AreEqual(1009.35,ing.exentos(100,30,15,"A"));
+		}
+
+	}
+}
+

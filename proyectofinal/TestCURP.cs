ca401e3edf64393affe541ad43bee99f9ea6eb46
CristinaGlz <cgm00001@gmail.com>
2014-11-27 19:46:52 -0600

    Update TestCURP.cs

--- proyectofinal/TestCURP.cs
+++ proyectofinal/TestCURP.cs
@@ -1,16 +1,30 @@
 using System;
-using Microsoft.VisualStudio.TestTools.UnitTesting;
+using NUnit.Framework;
 
-namespace UnitTestProject3
+namespace proyectofinal
 {
-    [TestClass]
+   [TestFixture ()]
     public class TestCURP
     {
         ObtenCURP curp = new ObtenCURP();
-        [TestMethod]
+         [Test()]
         public void TestObtenCURP()
         {
-            Assert.AreEqual("MEZJ19910514HZSDRR00", curp.creaCURP("MEDINA", "ZARAGOZA","JORGE","19910514","H","Zacatecas"));
+            Assert.AreEqual("MEZJ910514HZSDRR00", curp.creaCURP("MEDINA", "ZARAGOZA","JORGE","1991,05,14","H","Zacatecas"));
+        }
+		 [Test()]
+        public void TestObtenCURP1()
+        {
+            Assert.AreEqual("GOMC920803MZSNRR00", curp.creaCURP("GONZALEZ", "MOREIRA","CRISTINA","1992,08,03","M","Zacatecas"));
+        }
+		 [Test()]
+        public void TestObtenCURP2()
+        {
+            Assert.AreEqual("AAPR630321HDFLRC00", curp.creaCURP("ALAMAN", "PEREZ","RICARDO","1963,03,21","H","Distrito Federal"));
         }
     }
 }
+
+  
+      
+	

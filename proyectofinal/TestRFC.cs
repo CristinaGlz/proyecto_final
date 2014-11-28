c2b72938e9f8ff89aa17dfd67d1551304ba58ce7
jorgitopeke <jorgito.c.2011@gmail.com>
2014-11-28 03:29:50 -0600

    Update TestRFC.cs

--- proyectofinal/TestRFC.cs
+++ proyectofinal/TestRFC.cs
@@ -14,7 +14,7 @@ namespace proyectofinal
         }
         [Test ()]
         public void testObtenRFC1(){
-            Assert.AreEqual("GOMC920803XX0", rfc.obtenRFC("CRISTINA", "GONZALEZ", "MOREIRA", "03 de agosto de 1992"));
+            Assert.AreEqual("GOMC920803XX0", rfc.obtenRFC("GONZALEZ", "MOREIRA","CRISTINA", "03 de agosto de 1992"));
 
         }
        
